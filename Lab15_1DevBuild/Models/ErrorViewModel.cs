8e3c590c7ff7e6c77452ed04d34fa5e88218c453
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-09-27 15:26:03 -0400

    first commit

---
+++ Lab15_1DevBuild/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab15_1DevBuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
