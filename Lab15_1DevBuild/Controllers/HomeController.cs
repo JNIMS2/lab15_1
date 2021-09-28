8326add213f2367672369ffd9e1a1b473b8a5b07
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-09-27 21:43:59 -0400

    i got it working

--- Lab15_1DevBuild/Controllers/HomeController.cs
+++ Lab15_1DevBuild/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Lab15_1DevBuild.Controllers
@@ -22,6 +23,43 @@ namespace Lab15_1DevBuild.Controllers
         {
             return View();
         }
+
+
+        //get the shuffled new deck of cards
+        //this ur works
+        //replaced new with a deck id. need to figure out how to pass the New deck id to the draw 5
+        public async Task<IActionResult> GetDeck()
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = "/api/deck/9ol3rpkhq2ne/shuffle/?deck_count=1";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connnection = await client.GetAsync(path);
+            Root result = await connnection.Content.ReadAsAsync<Root>();
+            return View(result);
+        }
+
+        //draw 5 cards from that deck--display names and images inside a view
+        //maybe i need to make the root the already deckdraw
+        //url works without the .json. so, should i remove it and just put the deck id in key?
+        //or do i need the .json to show things? cuz i do that in razor...
+        //added the.json back in for now
+        public async Task<IActionResult> Drawfive(string deck_id)
+        { 
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/9ol3rpkhq2ne/draw/?count=5";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connnection = await client.GetAsync(path);
+            DeckDraw result = await connnection.Content.ReadAsAsync<DeckDraw>();
+            return View(result);
+        }
+
+
+        //have a button that draws 5 more cards
+
 
         public IActionResult Privacy()
         {
---
+++ Lab15_1DevBuild/Models/Deckofcards.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab15_1DevBuild.Models
+{
+    //renamed deckshuffled to Root
+    public class Root
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+    }
+
+    //the deck for this: 
+    
+//"success": true,
+//"deck_id": "30l9s5i22t2s",
+//"remaining": 52,
+//"shuffled": true
+
+
+    public class Card
+    {
+        public string code { get; set; }
+        public string value { get; set; }
+        public string suit { get; set; }
+    }
+
+    public class DeckDraw
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public List<Card> cards { get; set; }
+
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+    }
+}
